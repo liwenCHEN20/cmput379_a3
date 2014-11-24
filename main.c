3c9fdf3baa8bf30cc19e128e0960070eac5b157b
Corey Jamison <cjamison@ualberta.ca>
2014-11-24 11:29:31 -0800

    forgot to add previous changes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct process {
 	int* resources;
@@ -8,7 +9,15 @@ struct process {
 	int executionTime;
 };
 
+void printArray(int* array, int length) {
+	printf("\n");
+	for(int i = 0; i < length; ++i) {
+		printf("%i: %i\n", i, array[i]);
+	}
+}
+
 int* getNumArray(int length);
+int* getNumArrayFromInput(char* input, int count);
 
 int main(int argc, char* args[]) {
 	char inputResourceCount[BUFSIZ];
@@ -35,7 +44,9 @@ int main(int argc, char* args[]) {
 
 	printf("Number of instances of each resource type: ");
 	scanf("%[^\n]s", inputResourceInstances);
-	resourceInstances = getNumArray(resourceCount);
+//	resourceInstances = getNumArray(resourceCount);
+	resourceInstances = getNumArrayFromInput(inputResourceInstances, resourceCount);
+	printArray(resourceInstances, resourceCount);
 
 	getchar();
 	printf("Number of processes: ");
@@ -44,10 +55,45 @@ int main(int argc, char* args[]) {
 
 	printf("\nrc: %s\nrn: %s\nri: %s\npc: %s\n", inputResourceCount, inputResourceNames, inputResourceInstances, inputProcessCount);
 
+	processes = malloc(sizeof(process) * processCount);
+	char processInput[BUFSIZ];
+	int processData[resourceCount + 2];
+	for(int i = 0; i < processCount; ++i) {
+		printf("Details of process %i:");
+		scanf("%s[^\n]s", processInput);
+		processData = getNumArrayFromInput(processInput, resourceCount + 2);
+		processes[i]
+
 
 
 
 	free(resourceInstances);
+}
+
+int* getNumArrayFromInput(char* input, int count) {
+	int* numArray = malloc(sizeof(int) * count);
+	char temp[BUFSIZ];
+	int tempLen = 0;
+	int current = 0;
+	
+	for(int i = 0; i < strlen(input); ++i) {
+		if(isdigit(input[i])) {
+			temp[tempLen] = input[i];
+			tempLen++;
+		} else if(isspace(input[i]) || iscntrl(input[i])) {
+			if(tempLen != 0) {
+				numArray[current] = atoi(temp);
+				current++;
+				tempLen = 0;
+				memset(temp,0,sizeof(temp)); 
+			}
+		}
+	}
+	if(current <= count && tempLen != 0) {
+		numArray[current] = atoi(temp);
+	}
+
+	return numArray;
 }
 
 int* getNumArray(int length) {
@@ -57,3 +103,5 @@ int* getNumArray(int length) {
 	}
 	return array;
 }
+
+
